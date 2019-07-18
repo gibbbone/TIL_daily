e2c093648c36387a94fd9855822762bfe4264a53
Giovanni Bonaccorsi <giovanni.bonaccorsi@gmail.com>
2019-07-18 15:56:50 +0200

    tests

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# TIL_daily
-Things I've learned everyday with no particular supervision 
+# TIL daily
+This is what happens when you don't have time to organize your notes but you need some place to dump them because you keep forgetting stuff: a big dump of markdown files stitched together.
---
+++ postit.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+# move to specific folder
+cd "$DESKTOP"/TIL_daily/notes
+
+# define empty post
+read -r -d '' POST << EOM
+##### Title
+Tag:
+
+Refs:
+Useful characters: \`,~
+---
+
+
+EOM
+
+TODAY=$(date +%Y-%m-%d)
+# check if file exist
+if [[ ! -f  $TODAY.md ]]; then
+    echo "####" $TODAY >> $TODAY.md
+    echo "$POST" >> $TODAY.md
+# else append
+else
+    echo "$POST" >> $TODAY.md
+fi
+
+micro $TODAY.md
