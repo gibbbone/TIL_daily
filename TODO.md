cf3b36b05f175af58ca70732122f8e59aa6a2fc9
Giovanni Bonaccorsi <giovanni.bonaccorsi@gmail.com>
2019-09-05 19:24:32 +0200

    2019-09-05.md

--- TODO.md
+++ TODO.md
@@ -1,12 +1,17 @@
+# TODO
 - add CLI arguments to
-	- just modify today note (i.e without adding another one)
-	- set starting folder
-	- open last note in a markdown viewer
+	- just modify today note (i.e without adding another one) `[X]`
+	- open last note in a markdown viewer `[X]`
+		- work only on Windows for the moment
+	- git shortcut: merge + add + commit + push `[X]` 
+- set execution attributes of the bash command `[X]`
 
+# TODO long run
 - how to stitch together different markdown files?
 	- a for loop with cat?
 		- must respect order: easy with dates
 	- `mdmerge`
 - decide: use collapsed file as README?
-- git shortcut: merge + add + commit + push
-- global bash command to invoke from any place `[X]`
+- configuration:
+	- set starting folder
+	- set editor of choice
--- postit.sh
+++ postit.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
-# move to specific folder
-cd "$DESKTOP"/TIL_daily/notes
+while getopts 'zrc' flag; do
+    case "${flag}" in
+        z) mode='modify_it' ;;
+        r) mode='read_it' ;;
+        c) mode='commit_it' ;;    
+        *) echo "Option: not recognized"
+            exit 1 ;;
+    esac
+done
 
 # define empty post
 read -r -d '' POST << EOM
@@ -16,14 +23,53 @@ Useful characters: \`,~
 EOM
 
 TODAY=$(date +%Y-%m-%d)
-# check if file exist
-if [[ ! -f  $TODAY.md ]]; then
-    echo "##" $TODAY >> $TODAY.md
-    echo "---" >> $TODAY.md    
-    echo "$POST" >> $TODAY.md
-# else append
-else
-    echo "$POST" >> $TODAY.md
-fi
+if [ $# -eq 0 ]; then    
+    # move to specific folder
+    cd "$DESKTOP"/TIL_daily/notes
 
-micro $TODAY.md:10000
+    # check if file exist
+    if [[ ! -f  $TODAY.md ]]; then
+        echo "##" $TODAY >> $TODAY.md
+        echo "---" >> $TODAY.md    
+        echo "$POST" >> $TODAY.md
+    # else append
+    else
+        echo "$POST" >> $TODAY.md
+    fi
+    micro $TODAY.md:10000
+else
+    if [[ ! -f  "$DESKTOP"/TIL_daily/notes/"$TODAY".md ]]; then
+        echo "Error. Cannot run with options: file does not exists."
+        exit 1
+    else
+        case "$mode" in
+            modify_it)        
+                cd "$DESKTOP"/TIL_daily/notes
+                micro $TODAY.md:10000
+                ;;
+            read_it)
+                cd "$DESKTOP"/TIL_daily/notes
+                cmd.exe /C start $TODAY.md
+                exit 1
+                ;;
+            commit_it)
+                echo "What do you wish to do? (provide number)"
+                select yn in "Commit all changes" "Abort"; do
+                    case "$yn" in
+                        "Commit all changes" ) 
+                            cd "$DESKTOP"/TIL_daily
+                            git add *
+                            git commit -m $TODAY.md
+                            git push
+                            break
+                            ;;
+                        "Abort" ) 
+                            break
+                            ;;
+                    esac
+                done
+                exit
+                ;;
+        esac
+    fi
+fi
